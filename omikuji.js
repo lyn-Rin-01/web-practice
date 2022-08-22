14fe96a64280c0340895daee1db2aee5aaedeb6a
lyn-Rin-01 <111443012+lyn-Rin-01@users.noreply.github.com>
2022-08-22 22:36:58 +0900

    修正　omikuji.js

--- omikuji.js
+++ omikuji.js
@@ -1,22 +1,32 @@
+let name;
 let username;
 let userresult;
-username = prompt("お名前を教えて下さい。");
-document.getElementById("").innerHTML = username;
 
-let rand = Math.floor( Math.random() * 5) ;
+username = prompt("お名前を教えて下さい。");
+
+if (username == "") {
+  document.getElementById("名無し").innerHTML = username;
+}
+
+if (username == "name") {
+  username == "name";
+}
+document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor( Math.random() * 5);
 if (rand == 0) {
-    userresult = "大吉";
-  }
-  if (rand == 1) {
-    userresult = "中吉";
-  }
-  if (rand == 2) {
-    userresult = "小吉";
-  }
-  if (rand == 3) {
-    userresult = "吉";
-  }
-  if (rand == 4) {
-    userresult = "凶";
-  }
-  document.getElementById("result").innerHTML = userresult;
+  userresult = "大吉";
+}
+if (rand == 1) {
+  userresult = "中吉";
+}
+if (rand == 2) {
+  userresult = "小吉";
+}
+if (rand == 3) {
+  userresult = "吉";
+}
+if (rand == 4) {
+  userresult = "凶";
+}
+document.getElementById("result").innerHTML = userresult;
